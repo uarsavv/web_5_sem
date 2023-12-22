3bda51fe2e0409f92c02cfcae1c09eaf83aa2f4b
Iva <uarovaiva@gmail.com>
2023-12-22 16:23:01 +0300

    add  4labs and fixed 3labs

---
+++ js/time.js
@@ -0,0 +1,27 @@
+(function () {
+     function measureLoadTime() { //используем для измерения времени загрузки страницы
+        const startTime = window.performance.now();
+
+
+    window.addEventListener('load', function() { //будет выполнен при загрузке страницы
+        const loadTime = window.performance.now() - startTime;
+        displayLoadStatistics(loadTime); // отвечает за за отображение статистики загрузки
+    });
+}
+
+    function formatTime(milliseconds) {
+        const seconds = (milliseconds / 1000).toFixed(3); //преобразует время загрузки из миллесекунд в секунды и форматирует его
+        return seconds + " сек";
+    }
+
+    function displayLoadStatistics(loadTime) {
+        const formattedLoadTime = formatTime(loadTime);
+
+        const footer = document.createElement('footer');
+        footer.innerHTML = "<p>Время загрузки страницы: " + performance.now() + "</p>";
+
+        document.body.appendChild(footer);//добавляет узел в конец списка дочерних элементов указанного родительского узла. элемент футера добавляем к элементу бади
+    }
+
+    measureLoadTime(); // запускает измерение времени загрузки при загрузке страницы.
+})();
