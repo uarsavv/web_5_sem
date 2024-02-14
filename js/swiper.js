dc836fa5303c02629b5d7a462888001c0fc2ceea
Iva <uarovaiva@gmail.com>
2024-02-14 20:24:55 +0300

    add function in swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -2,25 +2,25 @@ var swiper = new Swiper(".swiper", {
     spaceBetween: 20,
     slidesPerView: 1,
     centeredSlides: true,
-
+    // включаем автоматическую прокрутку слайдов с задержкой
     autoplay: {
         delay: 2500,
     },
-
+    // включаем скроллбар с возможностью его перетаскивания
     scrollbar: {
         el: '.swiper-scrollbar',
-        draggable: true,
+        draggable: true, // штучка которая позволяет их перетаскивать с помощью мыши
 
     },
-
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
     },
 
+    // устанавливаем эффект перелистывания слайдов как "flip"
     effect: 'flip',
     flipEffect: {
         slideShadows: false,
     },
-
+    allowTouchMove: false,
 });
